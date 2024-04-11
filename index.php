2b831fcb4e31dd70dd0db3c57d22e36f73e50bc8
Medaille13 <110163029+Medaille13@users.noreply.github.com>
2024-04-11 12:57:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link href="./style/main.css<?php echo '?v='.time(); ?>" rel="stylesheet" type="text/css" />
+    <title>Document</title>
+</head>
+<body>
+<header id="barmenu" class="header">
+    <div class="menu-total">
+    <div class="logo">
+    <a href=""><img src="./image/logo.jpg" alt="" class="logomenu"></img></a>
+    </div>
+    <nav id="menu-haut">
+    <a href="#section1" class="menu" >Présentation</a>
+    <a href="#section2" class="menu" >La résidence</a>
+    <a href="#section1" class="menu" >Les services</a>
+    <a href="#section1" class="menu" >Actualités</a>
+    <a href="#section7" class="menu" id="contactmenu">Contact</a>
+    <a href="tel:0160749300" class="menu always" >01 60 74 93 00</a>
+    </nav>
+    </div>
+</header>
+<section>
+<h1></h1>
+
+<!-- Slider main container -->
+<div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div>
+
+
+
+
+</section>
+  
+  
+<footer>
+  <div class="footer-total">
+<div>© 2020 - LogiRys</div><nav class="footer-bas"><aside>|</aside><a href="">Mentions légales </a><aside>|</aside><a href="">Politique de protection des données</a></nav>
+  </div>
+</footer>
+<script src="./javascript/app.js"></script>
+</body>
+</html>
---
+++ index_old.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link href="./style/main.css<?php echo '?v='.time(); ?>" rel="stylesheet" type="text/css" />
+    <title>Document</title>
+</head>
+<body>
+<header id="barmenu" class="header">
+    <div class="menu-total">
+    <div class="logo">
+    <a href=""><img src="./image/logo.jpg" alt="" class="logomenu"></img></a>
+    </div>
+    <nav id="menu-haut">
+    <a href="#section1" class="menu" >Présentation</a>
+    <a href="#section2" class="menu" >La résidence</a>
+    <a href="#section1" class="menu" >Les services</a>
+    <a href="#section1" class="menu" >Actualités</a>
+    <a href="#section7" class="menu" id="contactmenu">Contact</a>
+    <a href="tel:0160749300" class="menu always" >01 60 74 93 00</a>
+    </nav>
+    </div>
+</header>
+
+    <article>
+        <!--<div class="swiper-container"> -->
+    <!-- Additional required wrapper -->
+     <!--<div class="parallax-bg"></div>
+    <div class="swiper-wrapper"> -->
+        <!-- Slides -->
+         <!--<div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+        
+    </div> -->
+    <!-- If we need pagination -->
+    <!-- <div class="swiper-pagination"></div> -->
+
+    <!-- If we need navigation buttons -->
+    <!-- <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div> -->
+
+    <!-- If we need scrollbar -->
+    <!-- <div class="swiper-scrollbar"></div>
+    </div>-->
+    <!--
+    <div id="firstparagraph">
+    <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1
+        <div class="img-box">
+            <img src="./image/resid1.jpg" alt="">
+        </div>
+      </div>
+      <div class="swiper-slide">Slide 2
+      <div class="img-box">
+            <img src="./image/resid2.jpg" alt="">
+        </div>
+      </div>
+      <div class="swiper-slide">Slide 3
+      <div class="img-box">
+            <img src="./image/resid3.jpg" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-pagination"></div>
+  </div>
+
+
+  <h2>Votre résidence autonomie à Avon</h2>
+
+    <p>Un environnement de qualité pour une vie adaptée aux besoins des seniors autonomes.</p>
+    </div>
+
+
+    <section id="section2">
+    <article id="contact">
+    <h3 class="h3-center">Un concept accessible,pour les seniors aux envies de liberté et de sécurité</h3>
+    <img src="./image/visuel_1.jpg" id="dansevieux">
+    <p class="section2-text-gauche">Short explanation of your company's partnership program and policy. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas metus metus, in bibendum lacus rutrum vitae. Nam posuere est ut odio efficitur, id tristique orci consectetur.</p>
+    <br><h4>See Immediate Benefits</h4>
+    <br><p>Maecenas id lectus sed arcu hendrerit tincidunt. Duis feugiat magna at gravida finibus. Aenean dapibus tincidunt dolor ac finibus. Donec vel ligula dui. Sed dignissim dui eget consectetur sodales.</p>
+    <div style="clear:both"></div>
+    </article>
+    </section>
+
+    <section id="section3">
+    <article id="contact">
+    <h3>La Résidence Jean Fontenelle</h3><br>
+    <p>Située au sein du vieil Avon, la résidence Jean Fontenelle offre à ses habitants une haute qualité de vie au coeur d'un
+    emplacement prisé et facilement accessible.</p>
+    <br>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11617.776126113287!2d5.364380767048076!3d43.28402206147241!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0c976b89df5%3A0x89254f95b5ee2ded!2sBasilique%20Notre-Dame%20de%20la%20Garde!5e0!3m2!1sfr!2sfr!4v1710327715247!5m2!1sfr!2sfr" width="690" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div class="iframe-textdroite">
+
+      <div class="swiper mySwiper swiper2">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="img-box">
+            <img src="./image/plaquette-immobiliere.jpg" alt="">
+        </div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="img-box2">
+            <img src="./image/actualites.jpg" alt="">
+        </div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="img-box3">
+            <img src="./image/conseille.jpg" alt="">
+        </div>
+    </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-pagination"></div>
+    </div>
+    <p class="texte-droite-miniswiper">La résidence fait peau neuve en 2020 grâce à un projet de rénovation ambitieux qui assure un confort optimal aux habitants et lieu de vie actuel. Une terrasse et un salon climatisé ont notamment été créés pour assurer le bien
+      être de chacun lors des beaux jours. La résidence Jean-Fontenelle propose des logements  entièrement refaits à neuf, allant du T1Bis au T2, dont certains sont destinés aux personnes à mobilité réduite, en  rez-de-chaussée</p>
+      </div>
+      <div style="clear:both"></div>
+
+  </article>
+    </section>
+
+    <section id="section4">
+    <article id="contact">
+    <h2>Participez aux Rendez-vous découvertes</h2>
+    <p class="paragraphe4-center">Dès le mois de mars 2021, la résidence Jean-Fontenelle vous ouvre ses portes ! Venez rencontrer notre équipe d'encadrants et animateurs, visitez nos appartements témoins et profitez du cadre de vie des résidents. Les mesures sanitaires adéquates ont été mises en place pour la protection de tous. Au plaisir de vous rencontrer !</p>
+    </article>
+    </section>
+
+    <section id="section5">
+    <article id="contact"></article>
+    </section>
+
+    <section id="section6">
+    <article id="contact"></article>
+    </section>
+
+    <section id="section7">
+        <h4>dernieres actualités de la résidence</h4>
+        <article class="cardActualite"></article>
+        <article class="cardActualite2"></article>
+        <article class="cardActualite3"></article>
+    <article id="contact"></article>
+    </section>
+
+    <section id="section8">
+        <h2>Contactez-nous</h2>
+        <article class="card">
+
+        <div class="card">
+        <img src="img_avatar.png" alt="Avatar" style="width:100%">
+        <div class="container">
+        <h4><b>Contactez la résidence</b></h4>
+        <p>Nos équipes sont à votre disposition pour répondre à toutes vos questions</p>
+        <h3>Résidence Jean Fontenelle</h3>
+        <p>16, rue Jean Fontelle 77120 AVON Du lundi au vendredi de 9h à 12h30 et de 14h30 à 19h.</p>
+        </div>
+        </div> 
+
+        </article>
+
+
+
+        <article class="formulaire">
+            <h3>Vous souhaitez</h3>
+        <div>
+        <input type="checkbox" id="rappel" name="rappel" checked />
+        <label for="rappel">Etre rappelé</label>
+        </div>
+
+        <div>
+            <input type="checkbox" id="visite" name="visite" />
+            <label for="visite">Organiser une visite</label>
+        </div>
+        </fieldset>
+        <label for="name">Nom&nbsp;:</label>
+        <input type="text" id="name" name="user_name" />
+        <label for="name">Prénom&nbsp;:</label>
+        <input type="text" id="name" name="user_name" />
+        <label for="name">Email&nbsp;:</label>
+        <input type="email" id="email" name="email" />
+        <label for="name">Numéro de téléphone&nbsp;:</label>
+        <input type="text" id="name" name="user_name" />
+        <div><b>Recherche</b></div>
+        <input type="checkbox" id="t1" name="t1" checked />
+        <label for="t1">t1</label>
+        </div>
+
+        <div>
+            <input type="checkbox" id="t2" name="t3" />
+            <label for="t2">t2</label>
+        </div>
+
+        <div>
+            <input type="checkbox" id="t3" name="t3" />
+            <label for="t3">t3</label>
+        </div>
+
+        <input type="submit" value="Envoyer" />
+
+        </article>
+
+    <article id="sociaux">Suivez-nous sur les réseaux sociaux</article>
+      -->
+    </section>
+
+        <!--<div class="fr-grid-row fr-grid-row--right">
+        <a class="fr-link fr-icon-arrow-up-fill fr-link--icon-left" href="#top">Haut de page</a>
+        </div>-->
+  
+<footer>
+  <div class="footer-total">
+<div>© 2020 - LogiRys</div><nav class="footer-bas"><aside>|</aside><a href="">Mentions légales </a><aside>|</aside><a href="">Politique de protection des données</a></nav>
+  </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script>
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+</script>
+<script src="./javascript/app.js"></script>
+</body>
+</html>
