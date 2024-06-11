954b7c605830a7099b7865216d32861f54af944a
Medaille13 <110163029+Medaille13@users.noreply.github.com>
2024-06-11 06:44:34 +0200

    indent perfect

--- index.php
+++ index.php
@@ -1,209 +1,247 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <link href="./node_modules/swiper/swiper-bundle.min.css" rel="stylesheet">
-    <link href="./style/main.css<?php echo '?v='.time(); ?>" rel="stylesheet" type="text/css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <link href="./node_modules/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="./style/main.css<?php echo '?v=' . time(); ?>" rel="stylesheet" type="text/css">
+  <title>Document</title>
 </head>
+
 <body>
-<header id="barmenu" class="header">
+  <header id="barmenu" class="header">
     <div class="menu-total">
-    <div class="logo">
-    <a href=""><img src="./image/logo.jpg" alt="" class="logomenu"></a>
-    </div>
-    <nav id="menu-haut">
-    <a href="#section1" class="menu" >Présentation</a>
-    <a href="#section2" class="menu" >La résidence</a>
-    <a href="#section1" class="menu" >Les services</a>
-    <a href="#section1" class="menu" >Actualités</a>
-    <a href="#section7" class="menu" id="contactmenu">Contact</a>
-    <a href="tel:0160749300" class="menu always" >01 60 74 93 00</a>
-    </nav>
-    </div>
-</header>
-<section>
-<article class="article1">
-
-<!-- Slider main container -->
-<div class="swiper" id="swipe-header">
-  <!-- Additional required wrapper -->
-  <div class="swiper-wrapper">
-    <!-- Slides -->
-        <img src="./image/resid1.jpg" alt="" class="swiper-slide">
-        <img src="./image/resid2.jpg" alt="" class="swiper-slide">
-        <img src="./image/resid3.jpg" alt="" class="swiper-slide">
-  </div>
-  <!-- If we need pagination -->
-  <div class="swiper-pagination"></div>
-
-  <!-- If we need navigation buttons -->
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
-
-</div>
-
-<div class="content-1">
-<h1>Votre résidence autonomie à Avon</h1>
-
-<p>Un environnement de qualité pour une vie adaptée aux besoins des seniors autonomes.</p>
-</div>
-
-</article>
-</section>
-
-<section>
-  <article class="article2">
-<h2>Un concept accessible, pour les seniors aux envies de liberté et de sécurité</h2>
-
-<div class="boite">
-<figure>
-<img src="./image/visuel_1.jpg" alt="">
-</figure>  
-<div class="contenu_1">
-<p>Short explanation of your company's partnership 
-program and policy. Lorem ipsum dolor sit amet, 
-consectetur adipiscing elit. Donec egestas metus 
-metus, in bibendum lacus rutrum vitae. Nam posuere
-est ut odio efficitur, id tristique orci consectetur.</p>
- 
-<h3>See Immediate Benefits</h3> 
-
-<p>Maecenas id lectus sed arcu hendrerit tincidunt. Duis 
-feugiat magna at gravida finibus. Aenean dapibus 
-tincidunt dolor ac finibus. Donec vel ligula dui. Sed 
-dignissim dui eget consectetur sodales.</p> 
-
-</div>
-</div>
-  </article>  
-</section>
-
-<section>
-  <article class="article3">
-    <h2>La Résidence Jean Fontenelle</h2>
-      <div>Située au sein du vieil Avon, la résidence Jean Fontenelle offre à ses habitants une haute qualité de vie au coeur d'un
-      emplacement prisé et facilement accessible.</div>
-      <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11616.788867339683!2d5.403443199999999!3d43.289191450000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1713997811370!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div class="logo">
+        <a href=""><img src="./image/logo.jpg" alt="" class="logomenu"></a>
       </div>
-    
+      <nav id="menu-haut">
+        <a href="#section1" class="menu">Présentation</a>
+        <a href="#section2" class="menu">La résidence</a>
+        <a href="#section1" class="menu">Les services</a>
+        <a href="#section1" class="menu">Actualités</a>
+        <a href="#section7" class="menu" id="contactmenu">Contact</a>
+        <a href="tel:0160749300" class="menu always">01 60 74 93 00</a>
+      </nav>
+    </div>
+  </header>
+  <section>
+    <article class="article1">
 
-<div class="boite2">
+      <!-- Slider main container -->
+      <div class="swiper" id="swipe-header">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+          <!-- Slides -->
+          <img src="./image/resid1.jpg" alt="" class="swiper-slide">
+          <img src="./image/resid2.jpg" alt="" class="swiper-slide">
+          <img src="./image/resid3.jpg" alt="" class="swiper-slide">
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
 
-<!-- Slider main container -->
-<div class="swiper swiper2" id="swiper-mini">
-  <!-- Additional required wrapper -->
-  <div class="swiper-wrapper">
-    <!-- Slides -->
-        <img src="./image/1.jpg" alt="" class="swiper-slide">
-        <img src="./image/2.jpg" alt="" class="swiper-slide">
-        <img src="./image/3.jpg" alt="" class="swiper-slide">
-  </div>
-  <!-- If we need pagination -->
-  <div class="swiper-pagination2"></div>
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
 
-  <!-- If we need navigation buttons -->
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
-  
-</div>
+      </div>
 
-<div class="contenu_2">
-  <p>
-      La résidence fait peau neuve en 2020 grâce à un projet de
-    rénovation ambitieux qui assure un confort optimal aux
-    habitants et lieu de vie actuel. Une terrasse et un salon
-    climatisé ont notamment été créés pour assurer le bien
-    être de chacun lors des beaux jours. <br><br>
+      <div class="content-1">
+        <h1>Votre résidence autonomie à Avon</h1>
 
-      La résidence Jean-Fontenelle propose des logements
-    entièrement refaits à neuf, allant du T1Bis au T2, dont
-    certains sont destinés aux personnes à mobilité réduite, en
-    rez-de-chaussée.
-  </p>
-  <a href="https://sentry.io/answers/">
-  <button>Consulter la brochure</button>
-</a>
-</div>
-</div>
-  </article>
+        <p>Un environnement de qualité pour une vie adaptée aux besoins des seniors autonomes.</p>
+      </div>
 
-</section>
+    </article>
+  </section>
 
-<section>
-  <article class="article4">
-  <div class="boite3">
-  <h2>Participez aux Rendez-vous découvertes</h2>
+  <section>
+    <article class="article2">
+      <h2>Un concept accessible, pour les seniors aux envies de liberté et de sécurité</h2>
 
-  <div class="contenu3"></div>
-  <p> Dès le mois de mars 2021, la résidence Jean-Fontenelle vous ouvre ses portes !
-Venez rencontrer notre équipe d'encadrants et animateurs, visitez nos appartements témoins
-et profitez du cadre de vie des résidents.
-Les mesures sanitaires adéquates ont été mises en place pour la protection de tous.
-Au plaisir de vous rencontrer ! </p>
-</div>
-<div class="button1">
+      <div class="boite">
+        <figure>
+          <img src="./image/visuel_1.jpg" alt="">
+        </figure>
+        <div class="contenu_1">
+          <p>Short explanation of your company's partnership
+            program and policy. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Donec egestas metus
+            metus, in bibendum lacus rutrum vitae. Nam posuere
+            est ut odio efficitur, id tristique orci consectetur.</p>
 
-</div>
+          <h3>See Immediate Benefits</h3>
 
-  </article>
-</section>
+          <p>Maecenas id lectus sed arcu hendrerit tincidunt. Duis
+            feugiat magna at gravida finibus. Aenean dapibus
+            tincidunt dolor ac finibus. Donec vel ligula dui. Sed
+            dignissim dui eget consectetur sodales.</p>
 
-<section>
-  <article class="article5">
+        </div>
+      </div>
+    </article>
+  </section>
 
-  </article>
-</section>
+  <section>
+    <article class="article3">
+      <h2>La Résidence Jean Fontenelle</h2>
+      <div>Située au sein du vieil Avon, la résidence Jean Fontenelle offre à ses habitants une haute qualité de vie au coeur d'un
+        emplacement prisé et facilement accessible.</div>
+      <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11616.788867339683!2d5.403443199999999!3d43.289191450000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1713997811370!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
 
-<section>
-  <article class="article6">
-<h2>Dernières actualités de la résidence</h2>
-<div class="slide-container">
-  <div class="slide-content">
-    <div class="card-wrapper">
-      <div class="card">
-        <div class="image-content">
-          <div class="overlay">
 
-          <div class="card-image">
-            <img src="" alt="" class="card-img">
+      <div class="boite2">
+
+        <!-- Slider main container -->
+        <div class="swiper swiper2" id="swiper-mini">
+          <!-- Additional required wrapper -->
+          <div class="swiper-wrapper">
+            <!-- Slides -->
+            <img src="./image/1.jpg" alt="" class="swiper-slide">
+            <img src="./image/2.jpg" alt="" class="swiper-slide">
+            <img src="./image/3.jpg" alt="" class="swiper-slide">
           </div>
-          </div>
-          <div class="card-content">
-            <h3>Mai 24, 2024</h3>
-            <input
-            type="datetime-local"
-            id="meeting-time"
-            name="meeting-time"
-            value="2018-06-12T19:30"
-            min="2018-06-07T00:00"
-            max="2018-06-14T00:00" />
-            <p class="description">Proven Health & Lifestyle Hacks That Will Boost Your Energy at Home and Work</p>
-            <button class="button-card">Read more</button>
+          <!-- If we need pagination -->
+          <div class="swiper-pagination2"></div>
+
+          <!-- If we need navigation buttons -->
+          <div class="swiper-button-prev"></div>
+          <div class="swiper-button-next"></div>
+
+        </div>
+
+        <div class="contenu_2">
+          <p>
+            La résidence fait peau neuve en 2020 grâce à un projet de
+            rénovation ambitieux qui assure un confort optimal aux
+            habitants et lieu de vie actuel. Une terrasse et un salon
+            climatisé ont notamment été créés pour assurer le bien
+            être de chacun lors des beaux jours. <br><br>
+
+            La résidence Jean-Fontenelle propose des logements
+            entièrement refaits à neuf, allant du T1Bis au T2, dont
+            certains sont destinés aux personnes à mobilité réduite, en
+            rez-de-chaussée.
+          </p>
+          <a href="https://sentry.io/answers/">
+            <button>Consulter la brochure</button>
+          </a>
+        </div>
+      </div>
+    </article>
+
+  </section>
+
+  <section>
+    <article class="article4">
+      <div class="boite3">
+        <h2>Participez aux Rendez-vous découvertes</h2>
+
+        <div class="contenu3">
+        Dès le mois de mars 2021, la résidence Jean-Fontenelle vous ouvre ses portes !
+          Venez rencontrer notre équipe d'encadrants et animateurs, visitez nos appartements témoins
+          et profitez du cadre de vie des résidents.
+          Les mesures sanitaires adéquates ont été mises en place pour la protection de tous.
+          Au plaisir de vous rencontrer !</div>
+      </div>
+      <div class="button1">
+
+      </div>
+
+    </article>
+  </section>
+
+  <section>
+    <article class="article5">
+      <h2>Des espaces de convivialité et des services sur-mesure</h2>
+      <div class="premier-bloc-menu">
+        <div class="premier-image-menu"></div>
+        <h3>Des activités favorisant l'autonomie</h3>
+        <p>Des activités hebdomadaires sont proposées et ont vocations à préserver l'autonomie des habitants : gym douce atelier mémoire, tai-chi, art floral...</p>
+      </div>
+
+      <div class="second-bloc-menu">
+        <div class="premier-image-menu"></div>
+        <h3>Un cadre naturellement propice aux sorties</h3>
+        <p>Des activités hebdomadaires sont proposées et ont vocations à préserver l'autonomie des habitants : gym douce atelier mémoire, tai-chi, art floral...</p>
+      </div>
+
+      <div class="troisieme-bloc-menu">
+        <div class="premier-image-menu"></div>
+        <h3>Une équipe expérimentée à vos côtés</h3>
+        <p>Service de téléassistance 7j/7, service de santé et maintien à domicile,</p>
+      </div>
+
+      <div class="slide-container-menu">
+        <div class="slide-content-menu">
+          <div class="card-wrapper-menu">
+            <div class="card-menu">
+              <div class="image-content-menu">
+                <div class="overlay-menu">
+
+                  <div class="card-image-menu">
+                    <img src="image/menu.jpg" class="card-img-menu">
+                  </div>
+                </div>
+                <div class="card-content-menu">
+                  <p class="description-menu">Du lundi au vendredi, le chef du restaurant géré par le CCAS d'Avon, vous propose un menu équilibré, de saison et des repas festifs tout au long de l'année.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+    </article>
+  </section>
+
+  <section>
+    <article class="article6">
+      <h2>Dernières actualités de la résidence</h2>
+      <div class="slide-container">
+        <div class="slide-content">
+          <div class="card-wrapper">
+            <div class="card">
+              <div class="image-content">
+                <div class="overlay">
+
+                  <div class="card-image">
+                    <img src="" alt="" class="card-img">
+                  </div>
+                </div>
+                <div class="card-content">
+                  <h3>Mai 24, 2024</h3>
+                  <input type="datetime-local" id="meeting-time" name="meeting-time" value="2018-06-12T19:30" min="2018-06-07T00:00" max="2018-06-14T00:00" />
+                  <p class="description">Proven Health & Lifestyle Hacks That Will Boost Your Energy at Home and Work</p>
+                  <button class="button-card">Read more</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </section>
+
+  <section>
+    <article class="article7">
+
+    </article>
+  </section>
+
+
+  <footer>
+    <div class="footer-total">
+      <div>© 2020 - LogiRys</div>
+      <nav class="footer-bas">
+        <aside>|</aside><a href="">Mentions légales </a>
+        <aside>|</aside><a href="">Politique de protection des données</a>
+      </nav>
     </div>
-  </div>
-</div>
-  </article>
-</section>
-
-<section>
-  <article class="article7">
-
-  </article>
-</section>
-  
-  
-<footer>
-  <div class="footer-total">
-<div>© 2020 - LogiRys</div><nav class="footer-bas"><aside>|</aside><a href="">Mentions légales </a><aside>|</aside><a href="">Politique de protection des données</a></nav>
-  </div>
-</footer>
-<script src="./node_modules/swiper/swiper-bundle.min.js"></script>
-<script src="./javascript/app.js" <?php echo '?v='.time(); ?>></script>
+  </footer>
+  <script src="./node_modules/swiper/swiper-bundle.min.js"></script>
+  <script src="./javascript/app.js" <?php echo '?v=' . time(); ?>></script>
 </body>
+
 </html>
